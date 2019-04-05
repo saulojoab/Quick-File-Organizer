6ddddb49b53a33e14c3227793dbc06d45adf7eb2
Saulo <saulojoabps@gmail.com>
2019-04-05 04:24:27 -0300

    listing music

--- Quick-File-Organizer/Quick-File-Organizer/Form1.Designer.cs
+++ Quick-File-Organizer/Quick-File-Organizer/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.btSelectFolder = new System.Windows.Forms.Button();
+            this.lblFilesFound = new System.Windows.Forms.Label();
+            this.lblMusic = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -56,12 +60,61 @@
             this.btSelectFolder.UseVisualStyleBackColor = false;
             this.btSelectFolder.Click += new System.EventHandler(this.btSelectFolder_Click);
             // 
+            // lblFilesFound
+            // 
+            this.lblFilesFound.AutoSize = true;
+            this.lblFilesFound.BackColor = System.Drawing.Color.Black;
+            this.lblFilesFound.Font = new System.Drawing.Font("Muli ExtraLight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFilesFound.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.lblFilesFound.Location = new System.Drawing.Point(318, 162);
+            this.lblFilesFound.Name = "lblFilesFound";
+            this.lblFilesFound.Size = new System.Drawing.Size(152, 26);
+            this.lblFilesFound.TabIndex = 2;
+            this.lblFilesFound.Text = "Files Found: 100";
+            // 
+            // lblMusic
+            // 
+            this.lblMusic.AutoSize = true;
+            this.lblMusic.Font = new System.Drawing.Font("Muli ExtraLight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMusic.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.lblMusic.Location = new System.Drawing.Point(188, 194);
+            this.lblMusic.Name = "lblMusic";
+            this.lblMusic.Size = new System.Drawing.Size(92, 26);
+            this.lblMusic.TabIndex = 3;
+            this.lblMusic.Text = "Music: 10";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Muli ExtraLight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(346, 196);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Videos: 10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Muli ExtraLight", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label3.Location = new System.Drawing.Point(498, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 26);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Documents: 10";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(794, 328);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblMusic);
+            this.Controls.Add(this.lblFilesFound);
             this.Controls.Add(this.btSelectFolder);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -75,6 +128,10 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btSelectFolder;
+        private System.Windows.Forms.Label lblFilesFound;
+        private System.Windows.Forms.Label lblMusic;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Quick-File-Organizer/Quick-File-Organizer/Form1.cs
+++ Quick-File-Organizer/Quick-File-Organizer/Form1.cs
@@ -18,6 +18,7 @@ namespace Quick_File_Organizer
             InitializeComponent();
         }
 
+
         private void btSelectFolder_Click(object sender, EventArgs e)
         {
             var fb = new FolderBrowserDialog();
@@ -27,6 +28,23 @@ namespace Quick_File_Organizer
             if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fb.SelectedPath))
             {
                 string[] files = Directory.GetFiles(fb.SelectedPath);
+
+                List<string> musicList = new List<string>();
+                List<string> documentList = new List<string>();
+                List<string> videoList = new List<string>();
+
+                foreach (string i in files)
+                {
+                    if (new[] { ".mp3", ".wav", ".ogg", ".flac", ".aac", ".wma" }.Any(c => i.EndsWith(c)))
+                    {
+                        musicList.Add(i);
+                    }
+                }
+
+                foreach (string i in musicList)
+                {
+                    Console.WriteLine(i);
+                }
 
                 MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
             } else
