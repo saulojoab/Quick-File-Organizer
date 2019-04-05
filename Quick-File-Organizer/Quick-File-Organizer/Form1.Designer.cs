00ba3e89463dbf8f18b9c556788d167fb4ad5d4a
Saulo <saulojoabps@gmail.com>
2019-04-05 03:49:55 -0300

    selecting folders and reading files

--- Quick-File-Organizer/Quick-File-Organizer/Form1.Designer.cs
+++ Quick-File-Organizer/Quick-File-Organizer/Form1.Designer.cs
@@ -28,13 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btSelectFolder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Muli ExtraLight", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(165, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(446, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quickly organize your files below:";
+            // 
+            // btSelectFolder
+            // 
+            this.btSelectFolder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btSelectFolder.Font = new System.Drawing.Font("Muli", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSelectFolder.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btSelectFolder.Location = new System.Drawing.Point(214, 84);
+            this.btSelectFolder.Name = "btSelectFolder";
+            this.btSelectFolder.Size = new System.Drawing.Size(367, 58);
+            this.btSelectFolder.TabIndex = 1;
+            this.btSelectFolder.Text = "Select Your Folder";
+            this.btSelectFolder.UseVisualStyleBackColor = false;
+            this.btSelectFolder.Click += new System.EventHandler(this.btSelectFolder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(794, 328);
+            this.Controls.Add(this.btSelectFolder);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "QFO - Quick File Organizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btSelectFolder;
     }
 }
 
--- Quick-File-Organizer/Quick-File-Organizer/Form1.cs
+++ Quick-File-Organizer/Quick-File-Organizer/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,23 @@ namespace Quick_File_Organizer
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btSelectFolder_Click(object sender, EventArgs e)
+        {
+            var fb = new FolderBrowserDialog();
+
+            DialogResult result = fb.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fb.SelectedPath))
+            {
+                string[] files = Directory.GetFiles(fb.SelectedPath);
+
+                MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+            } else
+            {
+                MessageBox.Show("You haven't selected any folders! :(", "OOPS!");
+            }
         }
     }
 }
